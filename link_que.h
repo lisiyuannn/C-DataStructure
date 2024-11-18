a6066a39d69afe0f5abab14a4f75823b39fe4e5b
aiit-jie_网关 <1353516321@qq.com>
2024-11-18 08:32:50 +0800

    临时数据，错误：无法解析的外部符号linkQueInit

---
+++ link_que.h
@@ -0,0 +1,30 @@
+#pragma once
+#include "config.h"
+
+typedef int LinkQueElemType;
+
+typedef struct LinkQueNode
+{
+	LinkQueElemType data;
+	struct LinkQueNode* next;
+}LinkQueNode, * LinkQuePtr;
+
+typedef struct
+{
+	struct LinkQueNode* front;
+	struct LinkQueNode* rear;
+}LinkQueList;
+
+//初始化队列
+void linkQueInit(LinkQueList*);
+
+//入队函数
+int pushLinkQue(LinkQueList* queue, LinkQueElemType data);
+
+//出队函数
+LinkQueElemType popLinkQue(LinkQueList* queue);
+
+//取栈顶元素
+LinkQueElemType getLinkQue(LinkQueList* queue);
+
+
--- main.c
+++ main.c
@@ -8,52 +8,20 @@
 #include "liner_stack.h"
 #include "link_stack.h"
 #include "liner_queue.h"
+#include "link_que.h"
 
 int main(void)
 {
-    //创建循环列表
-    LinerQueue* queue = NULL;
-    queue = initLinerQueue();
-    if (!queue)
+    LinkQueList* queue = (LinkQueList*)malloc(sizeof(LinkQueList));
+    if (queue == NULL)
     {
-        printf("循环队列初始化失败！\n");
-        return 0;
+        printf("链队列初始化失败！");
     }
-
-    printf("循环队列初始化成功！\n");
-
-    isEmptyQue(queue);
-
-    isFullQue(queue);
-
-    pushQueue(queue, 1);
-    pushQueue(queue, 2);
-    pushQueue(queue, 3);
-    pushQueue(queue, 4);
-    pushQueue(queue, 5);
-    pushQueue(queue, 6);
-    pushQueue(queue, 7);
-    pushQueue(queue, 8);
-    pushQueue(queue, 9);
-    pushQueue(queue, 10);
-
-    printf("%d\n", getFrontQue(queue));
-    printf("%d\n", getRearQue(queue));
-
-
-    printf("%d\n", popQueue(queue));
-    printf("%d\n", popQueue(queue));
-    printf("%d\n", popQueue(queue));
-    printf("%d\n", popQueue(queue));
-    printf("%d\n", popQueue(queue));
-    printf("%d\n", popQueue(queue));
-    printf("%d\n", popQueue(queue));
-    printf("%d\n", popQueue(queue));
-    printf("%d\n", popQueue(queue));
-    printf("%d\n", popQueue(queue));
-    printf("%d\n", popQueue(queue));
-    printf("%d\n", popQueue(queue));
-
+    else
+    {
+        printf("链队初始化成功！");
+    }
+    linkQueInit(queue);
 
     return 0;
 }
